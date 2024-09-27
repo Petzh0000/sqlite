c2a297cb0ad8994c6243c7bca2628e2acd015862
petzh0000 <petzh0000_@hotmail.com>
2024-09-27 11:37:36 +0700

    first commit

---
+++ lib/page/sqlite_page.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import '/services/database_helper.dart';
+
+class SqlitePage extends StatefulWidget {
+  const SqlitePage({super.key});
+
+  @override
+  State<SqlitePage> createState() => _SqlitePageState();
+}
+
+class _SqlitePageState extends State<SqlitePage> {
+  //dbHelper ซึ่งเป็นอินสแตนซ์ของ DatabaseHelper
+  //สําหรับจัดการฐานข้อมูล SQLite และสร้างตัวควบคุมสําหรับ TextField ที่จะใช้ในการ
+  //กรอกข้อมูลชื่อ (_nameController) และอีเมล (_emailController)
+  //users ใช้เก็บรายการข้อมูลที่ดึงมาจากฐานข้อมูล
+  //SQLite ในรูปแบบ List ของ Map ซึ่งแต่ละ Map แทนข้อมูลหนึ่งรายการ
+  DatabaseHelper dbHelper = DatabaseHelper();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  List<Map<String, dynamic>> users = [];
+
+  //สร้างฟังก์ชัน _addUser ที่ทําหน้าที่เพิ่มข้อมูลใหม่ลงในฐานข้อมูล โดยใช้
+  //ฟังก์ชัน insertUser ของ DatabaseHelper หลังจากเพิ่มข้อมูลสําเร็จ จะเคลียร์ข้อมูลใน
+  //ช่องกรอกเพื่อเตรียมพร้อมสําหรับการกรอกข้อมูลใหม่
+  Future<void> _addUser() async {
+    await dbHelper.insertUser({
+      'name': _nameController.text,
+      'email': _emailController.text,
+    });
+    _nameController.clear();
+    _emailController.clear();
+  }
+
+  Future<void> _updateUser(int id) async {
+    await dbHelper.updateUser({
+      'id': id,
+      'name': _nameController.text,
+      'email': _emailController.text,
+    });
+    _nameController.clear();
+    _emailController.clear();
+    _refreshUsers();
+  }
+
+  Future<void> _deleteUser(int id) async {
+    await dbHelper.deleteUser(id);
+    _refreshUsers(); // อัปเดตรายการข้อมูล
+  }
+
+  //สร้างฟอร์มเพิ่มข้อมูล โดยทําการสร้างฟังก์ชัน _showForm
+  void _showForm(int? id) {
+    if (id != null) {
+      final existingUser = users.firstWhere((element) => element['id'] == id);
+      _nameController.text = existingUser['name'];
+      _emailController.text = existingUser['email'];
+    }
+
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (_) {
+        return Padding(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+            top: 15,
+            left: 15,
+            right: 15,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: _nameController,
+                  decoration: InputDecoration(labelText: 'ชื่อ'),
+                ),
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(labelText: 'อีเมล'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (id == null) {
+                      _addUser();
+                    } else {
+                      _updateUser(id);
+                    }
+                    Navigator.of(context).pop(); // ปิด bottom sheet
+                  },
+                  child: Text(id == null ? 'เพิ่มข้อมูล' : 'แก้ไขข้อมูล'),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  //initState ซึ่งจะถูกเรียกใช้ทันทีเมื่อหน้าจอถูกสร้างขึ้น โดย
+  //ภายในเมธอดนี้จะมีการเรียกฟังก์ชัน _refreshUsers เพื่อดึงข้อมูลทั้งหมดจากฐานข้อมูล
+  //สร้างฟังก์ชัน _refreshUsers ทํางานโดยใช้เมธอด getUsers ของ DatabaseHelper เพื่อดึง
+  //ข้อมูลจากฐานข้อมูล เมื่อดึงข้อมูลเสร็จสิ้น ฟังก์ชันจะอัปเดตสถานะของตัวแปร users ด้วย
+  //ข้อมูลที่ดึงมาได้ และใช้ setState เพื่อบอกฟลัตเตอร์ว่าต้องการอัปเดตหน้าจอใหม่ตาม
+  //ข้อมูลที่เปลี่ยนแปลง
+  @override
+  void initState() {
+    super.initState();
+    _refreshUsers();
+  }
+
+  Future<void> _refreshUsers() async {
+    final data = await dbHelper.getUsers();
+    setState(() {
+      users = data;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        title: Text('รายการข้อมูล'),
+      ),
+      body: users.isEmpty
+          ? Center(
+              child: Text(
+                  'ไม่มีข้อมูล กรุณาเพิ่มข้อมูลใหม่'), // ข้อความแสดงที่กลางหน้าจอเมื่อไม่มีข้อมูล
+            )
+          : ListView.builder(
+              itemCount: users.length, // จํานวนข้อมูลในรายการ
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(users[index]['name']),
+                  subtitle: Text(users[index]['email']),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () => _showForm(users[index]['id']),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () => _deleteUser(users[index]['id']),
+                      ),
+                    ],
+                  )
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showForm(null),
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/services/database_helper.dart
@@ -0,0 +1,62 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  factory DatabaseHelper() => _instance;
+  static Database? _database;
+  DatabaseHelper._internal();
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'app_database.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT
+    )
+    ''');
+  }
+
+  Future<int> insertUser(Map<String, dynamic> user) async {
+    Database db = await database;
+    return await db.insert('users', user);
+  }
+
+  Future<int> updateUser(Map<String, dynamic> user) async {
+    Database db = await database;
+    return await db.update(
+      'users',
+      user,
+      where: 'id = ?',
+      whereArgs: [user['id']],
+    );
+  }
+
+  Future<int> deleteUser(int id) async {
+    Database db = await database;
+    return await db.delete(
+      'users',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<List<Map<String, dynamic>>> getUsers() async {
+    Database db = await database;
+    return await db.query('users');
+  }
+}
